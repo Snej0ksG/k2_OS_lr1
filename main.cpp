ee8ac4bdb4df9f28409fe79c35f2e885f2f5b6cf
noutlin <cbihka54@gmail.com>
2025-09-15 22:33:55 +0300

    1

--- main.cpp
+++ main.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
-using namespace std;
-int main() {
-    int *array = new int[1024];
+#include <cstddef>
+std::byte *array = new std::byte[1024];
 
-    return 0;
+void push(int &count) {
+    int replays = (count+64-1)/64;
+    for (int k = 0; k < replays; k++) {
+        for (int i = 0; i < 1024; ++i) {
+            if (*(array+i)==std::byte{0}) {
+                for (int j = i; j < i+64; ++j)
+                {
+                    if (j==i) *(array+i)=std::byte{2};
+                    else *(array+j)=std::byte{1};
+                }
+                break;
+            }
+        }
+    }
+}
+int main() {
+    while (true) {
+        std::string a;
+        int count = 0;
+        std::cout << "\n";
+        std::cin >> a;
+        if (a == "1") {
+            std::cin >> count;
+            push(count);
+        }
+        if (a == "2") {
+            for (int i = 0; i < 1024;) {
+                if (*(array+i)==std::byte{2}) {
+                    std::cout<<"1";
+                    i+=64;
+
+                }
+                else {
+                    std::cout<<"0";
+                    i+=64;
+                }
+            }
+        }
+    }
 }
